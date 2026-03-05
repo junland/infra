fff7fab54d327593b2745bcc2514f660758d6dbd
John Unland <unlandj2012@gmail.com>
2026-03-05 08:36:24 -0600

    fix(terraform): add missing newline for improved readability in k8s module

--- terraform/_k8s_testing.tf
+++ terraform/_k8s_testing.tf
@@ -2,6 +2,7 @@ module "k8s" {
   count = var.k8s_enable_module ? 1 : 0
 
   source                        = "./k8s"
+
   k8s_kubectl_config_path       = var.k8s_kubectl_config_path
   k8s_loadbalancer_ip           = var.k8s_loadbalancer_ip
   k8s_loadbalancer_pool         = var.k8s_loadbalancer_pool
