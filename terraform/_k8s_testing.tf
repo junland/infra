77cc3f276acde609d712bf68d9981b914c6d1168
John Unland <unlandj2012@gmail.com>
2026-03-09 08:53:35 -0500

    feat(terraform): refactor k8s LoadBalancer pool configuration and add validations

--- terraform/_k8s_testing.tf
+++ terraform/_k8s_testing.tf
@@ -5,7 +5,7 @@ module "k8s" {
 
   k8s_kubectl_config_path       = var.k8s_kubectl_config_path
   k8s_loadbalancer_ip           = var.k8s_loadbalancer_ip
-  k8s_loadbalancer_pool         = var.k8s_loadbalancer_pool
+  k8s_loadbalancer_pool         = local.k8s_pool_range
   k8s_cert_manager_email        = var.k8s_cert_manager_email
   k8s_cert_manager_domain       = var.k8s_cert_manager_domain
   k8s_longhorn_admin_password   = var.k8s_longhorn_admin_password
---
+++ terraform/_locals.tf
@@ -0,0 +1,3 @@
+locals {
+  k8s_pool_range = "${var.k8s_loadbalancer_pool_start}-${var.k8s_loadbalancer_pool_end}"
+}
